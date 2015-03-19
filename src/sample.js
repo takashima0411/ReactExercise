55eac237bfcddc8995f40533cdaae311afaf9d72
takashima <takashima0411@gmail.com>
2015-03-19 20:55:56 +0900

    feat/react.js

--- src/sample.js
+++ src/sample.js
@@ -1,19 +1,43 @@
-var championSquareTemplateURL = "http://ddragon.leagueoflegends.com/cdn/5.2.1/img/champion/<champion>.png ";
-
-$(function(){
-	var request = $.get('http://ddragon.leagueoflegends.com/cdn/5.2.1/data/en_US/champion.json');
-
-	request.done(function(json){
-		var champions = json.data;
-		for( var champion in json.data){
-			var img = $("<img>").attr("src",championSquareTemplateURL.replace("<champion>",champion));
-			var panelBody = $("<div>").addClass("panel-body").append($("<p>").text(champion)).append(img);
-			var panel = $("<div>").addClass("panel panel-default col-xs-6 col-sm-4 col-md-3 col-lg-2").append(panelBody);
-			$("#data").append(panel);
-		}
-	});
-
-	request.fail(function(msg){
-		$("#data").text('request failer');
-	});
+/** @jsx React.DOM */
+var MainComponent = React.createClass({
+	getInitialState : function(){
+		return {
+			championNodes : []
+		};
+	},
+	componentDidMount : function(){
+		var _this = this;
+		$.get('https://ddragon.leagueoflegends.com/cdn/5.2.1/data/en_US/champion.json',function(result){
+			var data = $.map(result.data,function(champion,hoge){
+				return (<ChampionComponent name={hoge} />);
+			});
+			_this.setState({ champions : data });
+		});
+	},
+	render : function() {
+		return (
+			<div className="MainComponent container-fluid">
+				<h1>Champions</h1>
+				<div id="data">
+					{this.state.champions}
+				</div>
+			</div>
+		);
+	}
 });
+
+var ChampionComponent = React.createClass({
+	render : function() {
+		var championImg = `http://ddragon.leagueoflegends.com/cdn/5.2.1/img/champion/${this.props.name}.png`
+		return (
+			<div className="champion panel panel-default col-xs-6 col-sm-4 col-md-3 col-lg-2">
+				<div className="panel-body">
+					<p>{this.props.name}</p>
+					<img src={championImg} />
+				</div>
+			</div>
+		)
+	}
+});
+
+React.render(<MainComponent />, document.body);
