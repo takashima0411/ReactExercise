3c3e642c0b10de5123b8d94f388b68fe676271e5
takashima <takashima0411@gmail.com>
2015-03-22 23:25:19 +0900

    feat/sort

--- src/sample.js
+++ src/sample.js
@@ -3,32 +3,45 @@ var MainComponent = React.createClass({
 	getInitialState : function(){
 		return {
 			filterText:'',
+			sortCondition:'name',
 			champions : []
 		};
 	},
 	handleUserIntput: function(filterText){
 		this.setState({ filterText: filterText});
 	},
+	handleSortCondition: function(sortCondition){
+		this.setState({ sortCondition: sortCondition});
+	},
 	componentDidMount : function(){
 		var _this = this;
 		$.get('https://ddragon.leagueoflegends.com/cdn/5.2.1/data/en_US/champion.json',function(result){
 			var data = $.map(result.data,function(champion,hoge){
-				return hoge;
+				return champion;
 			});
 			_this.setState({ champions : data });
 		});
 	},
 	render : function() {
 		var _this = this;
-		var data = $.map(this.state.champions,function(champion,hoge){
-			if( champion.toString().toLowerCase().search(_this.state.filterText.toLowerCase()) != -1 || _this.state.filterText === ""){
-				return (<ChampionComponent name={champion} />);
-			}
+		var champions = this.state.champions;
+		var filtered = champions.filter(function(value,index){
+			return value.name.toLowerCase().search(_this.state.filterText.toLowerCase()) != -1
+		});
+		if(this.state.sortCondition == "key"){
+			filtered.sort(function(a,b){ return a.key - b.key});
+		}
+		if(this.state.sortCondition == "name"){
+			filtered.sort(function(a,b){ return a.name - b.name})
+		}
+		var data = $.map(filtered,function(champion,hoge){
+			return (<ChampionComponent champion={champion} />);
 		});
 		return (
 			<div className="MainComponent container-fluid">
 				<h1>Champions</h1>
-				<FilterBar filterText={this.state.filterText} onUserInput={this.handleUserIntput}/>
+				<SortCondition onUserSelect={this.handleSortCondition}/>
+				<FilterInput filterText={this.state.filterText} onUserInput={this.handleUserIntput}/>
 				<div id="data">
 					{data}
 				</div>
@@ -39,11 +52,11 @@ var MainComponent = React.createClass({
 
 var ChampionComponent = React.createClass({
 	render : function() {
-		var championImg = `http://ddragon.leagueoflegends.com/cdn/5.2.1/img/champion/${this.props.name}.png`
+		var championImg = `http://ddragon.leagueoflegends.com/cdn/5.2.1/img/champion/${this.props.champion.id}.png`
 		return (
 			<div className="champion panel panel-default col-xs-6 col-sm-4 col-md-3 col-lg-2">
 				<div className="panel-body">
-					<p>{this.props.name}</p>
+					<p>{this.props.champion.name}</p>
 					<img src={championImg} />
 				</div>
 			</div>
@@ -51,7 +64,28 @@ var ChampionComponent = React.createClass({
 	}
 });
 
-var FilterBar = React.createClass({
+var SortCondition = React.createClass({
+	handleChange : function(){
+		this.props.onUserSelect(
+			this.refs.sortCondition.getDOMNode().value
+		);
+	},
+	render : function(){
+		return(
+			<form>
+				<div className="form-group">
+					<label>sort by</label>
+					<select name="sort-by" ref="sortCondition" onChange={this.handleChange}>
+						<option label="sort-by-name" value="name">name</option>
+						<option label="sort-by-key" value="key">key</option>
+					</select>
+				</div>
+			</form>
+		);
+	}
+});
+
+var FilterInput = React.createClass({
 	handleChange : function(){
 		this.props.onUserInput(
 			this.refs.filterTextInput.getDOMNode().value
@@ -60,7 +94,10 @@ var FilterBar = React.createClass({
 	render : function(){
 		return(
 			<form>
-				<input type="text" placeholder="filter..." ref="filterTextInput" value={this.props.filterText} onChange={this.handleChange} />
+				<div className="form-group">
+					<label htmlFor="filter">filter</label>
+					<input id="filter" className="form-control" type="text" placeholder="filter..." ref="filterTextInput" value={this.props.filterText} onChange={this.handleChange} />
+				</div>
 			</form>
 		);
 	}
